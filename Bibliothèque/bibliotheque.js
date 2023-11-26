0abf5aeb3d9fcaffee7797a141652ae87dec913c
Jimmy Beugnies <93620368+Zrtehhz@users.noreply.github.com>
2023-11-26 18:52:04 +0100

    update bibliotheque

--- Bibliothèque/bibliotheque.js
+++ Bibliothèque/bibliotheque.js
@@ -1,42 +1,89 @@
-document.getElementById('addVideo').addEventListener('click', function() {
-    const url = document.getElementById('youtubeUrl').value;
-    if (url) {
-        // Simple validation check for YouTube URL
-        const matchUrl = url.match(/(?:youtu\.be\/|youtube\.com(?:\/embed\/|\/v\/|\/watch\?v=|\/watch\?.+&v=))((\w|-){11})/);
-        if (matchUrl) {
-            // Getting the video ID which is the capturing group in the regex
-            const videoId = matchUrl[1];
-            addVideoToLocalStorage(videoId);
-            document.getElementById('youtubeUrl').value = ''; // Clear input field
-        } else {
-            alert("Veuillez entrer une URL YouTube valide.");
-        }
+$(document).ready(function () {
+    let playlists = JSON.parse(localStorage.getItem("playlists")) || [];
+  
+    function savePlaylists() {
+      localStorage.setItem("playlists", JSON.stringify(playlists));
     }
-});
-
-function addVideoToLocalStorage(videoId) {
-    // Assuming we store videos by an array of IDs
-    let videos = JSON.parse(localStorage.getItem('youtubeVideos')) || [];
-    if (videos.indexOf(videoId) === -1) { // Avoid duplicate videoIds
-        videos.push(videoId);
-        localStorage.setItem('youtubeVideos', JSON.stringify(videos));
-        renderVideos();
+  
+    function displayPlaylists() {
+      let container = $("#playlistContainer");
+      container.empty();
+      playlists.forEach((playlist, index) => {
+      let playlistDiv = $(`
+        <div class="card" data-index="${index}">
+          <img src="../Assets/default.png" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title" style="color: #000;">${playlist.name}</h5>
+            <a href="../Playlist/Playlist.html" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#playlistModal" data-playlist-index="${index}">Ouvrir</a>
+            <button type="button" class="btn btn-danger btn-sm deletePlaylistBtn" data-playlist-index="${index}">Supprimer</button>
+          </div>
+        </div>
+      `);
+      container.append(playlistDiv);
+      });
+  
+      // Gestionnaire d'événements pour les boutons de suppression
+      $('.deletePlaylistBtn').click(function(event) {
+        let playlistIndex = $(this).attr('data-playlist-index');
+        deletePlaylist(event, playlistIndex);
+      });
     }
-}
-
-function renderVideos() {
-    const videos = JSON.parse(localStorage.getItem('youtubeVideos')) || [];
-    const container = document.getElementById('playlistContainer');
-    container.innerHTML = ''; // Clear previous contents
-    videos.forEach(videoId => {
-        const link = document.createElement('a');
-        link.href = `../Lecteur/player.html?videoId=${videoId}`;
-        link.target = '_blank';
-        link.innerText = `Play Video ${videoId}`;
-        container.appendChild(link);
-        container.appendChild(document.createElement('br')); // Line break for readability
+  
+    function deletePlaylist(event, index) {
+      event.stopPropagation();
+      playlists.splice(index, 1);
+      savePlaylists();
+      displayPlaylists();
+    }
+  
+    function addMusicToPlaylist(playlistIndex, musicUrl) {
+      let playlist = playlists[playlistIndex];
+      if (playlist) {
+        playlist.songs.push(musicUrl);
+        savePlaylists();
+        displayPlaylistSongs(playlistIndex);
+      }
+    }
+  
+    function displayPlaylistSongs(playlistIndex) {
+      let playlist = playlists[playlistIndex];
+      let modalBody = $("#playlistModal .modal-body");
+      modalBody.empty();
+  
+      playlist.songs.forEach((songUrl) => {
+        let songDiv = $(`
+          <div class="song">
+            <span>${songUrl}</span>
+          </div>
+        `);
+        modalBody.append(songDiv);
+      });
+    }
+  
+    displayPlaylists();
+  
+    $('#createPlaylist').click(function () {
+      let playlistName = prompt("Entrez le nom de la playlist:");
+      if (playlistName) {
+        playlists.push({ name: playlistName, songs: [] });
+        savePlaylists();
+        displayPlaylists();
+      }
     });
-}
-
-// On page load, render the videos if any
-window.addEventListener('DOMContentLoaded', renderVideos);
+  
+    $('#playlistContainer').on('click', '.card', function () {
+      let index = $(this).attr('data-index');
+      let playlist = playlists[index];
+      let playlistModal = $('#playlistModal');
+      playlistModal.attr('data-playlist-index', index);
+      displayPlaylistSongs(index);
+    });
+  
+    $('#modalAddMusicBtn').click(function () {
+      let playlistIndex = $('#playlistModal').attr('data-playlist-index');
+      let musicUrl = prompt("Entrez l'URL de la musique YouTube:");
+      if (musicUrl) {
+        addMusicToPlaylist(playlistIndex, musicUrl);
+      }
+    });
+  });
