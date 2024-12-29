9ce99487df681b6fd9d2225e0c1c694a89a0e838
Jimmy Beugnies <93620368+Zrtehhz@users.noreply.github.com>
2024-12-29 07:36:39 +0100

    t

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -14,7 +14,6 @@ files {
     'style.css', -- Assure-toi que le chemin est correct si ton CSS est dans un sous-dossier
     'script.js',  -- Assure-toi que le chemin est correct si ton JS est dans un sous-dossier
     'Assets/**',  -- Inclut tous les fichiers dans le dossier Assets
-    'Bibliothèque/**', -- Inclut tous les fichiers dans le dossier Bibliothèque
     'Lecteur/**', -- Inclut tous les fichiers dans le dossier Lecteur
     'Recherche/**' -- Inclut tous les fichiers dans le dossier Recherche
 }
